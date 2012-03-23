85748fd9b7e1ad61bd0c1a1a486edcddd8eccf71
ray.ni <ray.ni@ge.com>
2012-03-23 18:10:00 +0800

    add a Array forEach to support IE, 8 or earlier version

--- ucijs/public/javascripts/board.js
+++ ucijs/public/javascripts/board.js
@@ -5,6 +5,34 @@ ALL_PIECES = 'rnbakabnrccpppppPPPPPCCRNBAKABNR';
 ROW_NUM = 10;
 COL_NUM = 9;
 
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.com/#x15.4.4.18
+if ( !Array.prototype.forEach ) {
+  Array.prototype.forEach = function( callback, thisArg ) {
+    var T, k;
+    if ( this == null ) {
+      throw new TypeError( " this is null or not defined" );
+    }
+    var O = Object(this);
+    var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+    if ( {}.toString.call(callback) != "[object Function]" ) {
+      throw new TypeError( callback + " is not a function" );
+    }
+    if ( thisArg ) {
+      T = thisArg;
+    }
+    k = 0;
+    while( k < len ) {
+      var kValue;
+      if ( k in O ) {
+        kValue = O[ k ];
+        callback.call( T, kValue, k, O );
+      }
+      k++;
+    }
+  };
+}
+
 Board = (function() {
 
   function Board(option, initFen) {
